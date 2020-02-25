37da66b96bfdde36fd5edab39d4bec2c63376350
zhongwei <zhongwei@unilumin.cn>
2020-02-25 17:30:15 +0800

    add JsonToJavaBean

--- src/test/java/com/unilumin/demo/DemoApplicationTests.java
+++ src/test/java/com/unilumin/demo/DemoApplicationTests.java
@@ -1,10 +1,7 @@
 package com.unilumin.demo;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.unilumin.entity.Body;
-import com.unilumin.entity.Destinaition;
 import com.unilumin.utils.ReadUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,23 +18,26 @@ class DemoApplicationTests {
     @Test
     public static void main(String []args){
 
-       String path = "E:\\办公文件\\ledparas_屏体信息2.json";
+       String path = "E:\\ledparas2.json";
         String s = ReadUtils.readJsonFile(path);
         Map<String,Object> body = JSON.parseObject(s);
         System.out.println("body==========="+body.toString());
-        Body body1 = (Body) body;
-        Destinaition destinaition = (Destinaition) body1.getDestination();
-        System.out.println("id============"+destinaition.getID());
+        Body bodyBean = JSON.toJavaObject(JSON.parseObject(s),Body.class);
+        System.out.println("bodyBean==========="+bodyBean.toString());
+        Map<String,Object> destinaition = (Map<String, Object>) body.get("DESTINATION");
+        System.out.println("id============"+destinaition.get("ID"));
+        System.out.println("device============"+destinaition.get("DEVICE"));
+        System.out.println("INTERFACES============"+destinaition.get("INTERFACES"));
 //
-//        JSONArray links = jobj.getJSONArray("links");
+        JSONArray links = jobj.getJSONArray("links");
 
-//        for (int i = 0 ; i < links.size();i++){
-//            JSONObject key1 = (JSONObject)links.get(i);
-//            String name = (String)key1.get("name");
-//            String url = (String)key1.get("url");
-//            System.out.println(name);
-//            System.out.println(url);
-//        }
+        for (int i = 0 ; i < links.size();i++){
+            JSONObject key1 = (JSONObject)links.get(i);
+            String name = (String)key1.get("name");
+            String url = (String)key1.get("url");
+            System.out.println(name);
+            System.out.println(url);
+        }
 
     }
 
