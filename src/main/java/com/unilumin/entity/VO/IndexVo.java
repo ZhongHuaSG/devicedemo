1729012d60605935aab9e6a7ca142679f09f19d9
zhongwei <zhongwei@unilumin.cn>
2020-02-28 17:23:50 +0800

    update index

---
+++ src/main/java/com/unilumin/entity/VO/IndexVo.java
@@ -0,0 +1,14 @@
+package com.unilumin.entity.VO;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class IndexVo {
+
+
+
+}
