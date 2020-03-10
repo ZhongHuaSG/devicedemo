c5008ff652e0237a2a1f6a0025ea14136596f058
zhongwei <zhongwei@unilumin.cn>
2020-03-10 18:08:46 +0800

    about vue.js

--- src/main/java/com/unilumin/DemoApplication.java
+++ src/main/java/com/unilumin/DemoApplication.java
@@ -2,12 +2,16 @@ package com.unilumin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    //打包成war包时的配置重写方法
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(DemoApplication.class);
+//    }
 }
