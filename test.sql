5c95f4441693c91b08a98b35b84075638d1a4fa7
zhongwei <zhongwei@unilumin.cn>
2020-03-19 18:36:36 +0800

    new sqlfile

--- test.sql
+++ test.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80016
 File Encoding         : 65001
 
-Date: 2020-03-16 18:14:12
+Date: 2020-03-19 18:35:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,13 +24,14 @@ CREATE TABLE `tb_permissions` (
   `permissions_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '功能名称',
   `permissions_explain` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '权限说明（中文）',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Records of tb_permissions
 -- ----------------------------
 INSERT INTO `tb_permissions` VALUES ('1', 'query', '信息查询');
 INSERT INTO `tb_permissions` VALUES ('2', 'add', '新增');
+INSERT INTO `tb_permissions` VALUES ('3', 'delete', '删除用户');
 
 -- ----------------------------
 -- Table structure for tb_role
@@ -59,14 +60,15 @@ CREATE TABLE `tb_role_permissions` (
   `permissions_id` int(11) DEFAULT NULL COMMENT '功能id',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Records of tb_role_permissions
 -- ----------------------------
 INSERT INTO `tb_role_permissions` VALUES ('1', '1', '1', '2020-03-16 16:32:20');
 INSERT INTO `tb_role_permissions` VALUES ('2', '1', '2', '2020-03-16 16:32:29');
-INSERT INTO `tb_role_permissions` VALUES ('3', '2', '1', '2020-03-16 16:32:38');
+INSERT INTO `tb_role_permissions` VALUES ('3', '1', '3', '2020-03-19 14:21:07');
+INSERT INTO `tb_role_permissions` VALUES ('5', '2', '1', '2020-03-19 14:21:11');
 
 -- ----------------------------
 -- Table structure for tb_user
@@ -74,16 +76,18 @@ INSERT INTO `tb_role_permissions` VALUES ('3', '2', '1', '2020-03-16 16:32:38');
 DROP TABLE IF EXISTS `tb_user`;
 CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`,`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_user
 -- ----------------------------
-INSERT INTO `tb_user` VALUES ('1', 'laowang', '112233');
-INSERT INTO `tb_user` VALUES ('2', 'laoli', '123456');
+INSERT INTO `tb_user` VALUES ('3', 'root', 'CDF8F6C81EC46BEC077A9629B948BB48', '2020-03-19 09:27:47');
+INSERT INTO `tb_user` VALUES ('10', 'zhangwei', '1F470ABDA924E8A0AAA30D48BEC226FB', '2020-03-19 13:39:16');
+INSERT INTO `tb_user` VALUES ('11', 'laowang', '1F470ABDA924E8A0AAA30D48BEC226FB', '2020-03-19 15:03:18');
 
 -- ----------------------------
 -- Table structure for tb_user_role
@@ -95,11 +99,11 @@ CREATE TABLE `tb_user_role` (
   `role_id` int(11) DEFAULT NULL COMMENT '角色id',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Records of tb_user_role
 -- ----------------------------
-INSERT INTO `tb_user_role` VALUES ('1', '1', '1', '2020-03-16 16:33:27');
-INSERT INTO `tb_user_role` VALUES ('2', '2', '2', '2020-03-16 16:33:39');
-INSERT INTO `tb_user_role` VALUES ('3', '1', '2', '2020-03-16 17:09:48');
+INSERT INTO `tb_user_role` VALUES ('5', '3', '1', '2020-03-19 09:27:47');
+INSERT INTO `tb_user_role` VALUES ('12', '10', '1', '2020-03-19 13:39:16');
+INSERT INTO `tb_user_role` VALUES ('13', '11', '2', '2020-03-19 15:03:18');
